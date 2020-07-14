1c4c8eee23e9f7ca4c04b1b9f21d165404bd700d
Federico Jaeggi <fritzjaeggi2109@gmail.com>
2020-07-14 15:09:35 -0300

    docs folder added

---
+++ docs/precache-manifest.60bbd3f0420e69176d7fb0843552a1e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "259d05d33f836144ddaed180de4410ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0f675cec99c41bcf43c",
+    "url": "/static/css/main.fb10415c.chunk.css"
+  },
+  {
+    "revision": "cec30beedda70fa405d7",
+    "url": "/static/js/2.e881aa95.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e881aa95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0f675cec99c41bcf43c",
+    "url": "/static/js/main.07647cd7.chunk.js"
+  },
+  {
+    "revision": "6a26a6a45f9e65ecd26f",
+    "url": "/static/js/runtime-main.0ba3af82.js"
+  }
+]);
